9b0dcfd5bae7bab6a9b2e7e5876479cfee161118
Peter Park <peter.park@amd.com>
2026-07-08 14:19:39 -0400

    Revert "[docs/7.14.0] Use "Instinct Design" theme (#813)"
    This reverts commit b7d8b0a6ba2972e63401ba95022b3dbcb5e4ef2f.
    
    remove announcement

--- docs/conf.py
+++ docs/conf.py
@@ -141,18 +141,17 @@ html_css_files = ["legacy/vllm-benchmark.css"]
 external_projects_current_project = "rocm"
 html_theme = "rocm_docs_theme"
 html_theme_options = {
-    # "announcement": f"This is ROCm {ROCM_VERSION} latest production stream release documentation. For the ROCm 7.13.0 technology preview release documentation release, refer to <a id='rocm-banner' href='https://rocm.docs.amd.com/en/7.13.0-preview/'>ROCm documentation</a>.",
-    "flavor": "instinct-design",
-    # "header_title": f"ROCm™ {ROCM_VERSION}",
-    # "header_link": f"https://rocm.docs.amd.com/en/7.13.0-preview/index.html",
-    # "version_list_link": f"https://rocm.docs.amd.com/en/7.13.0-preview/release/versions.html",
-    # "nav_secondary_items": {
-    #     "GitHub": "https://github.com/ROCm/ROCm",
-    #     "Community": "https://github.com/ROCm/ROCm/discussions",
-    #     "Blogs": "https://rocm.blogs.amd.com/",
-    #     "System and Infra Docs": "https://instinct.docs.amd.com/",
-    #     "Support": "https://github.com/ROCm/ROCm/issues/new/choose",
-    # },
+    "flavor": "generic",
+    "header_title": f"ROCm™ {ROCM_VERSION}",
+    "header_link": f"https://rocm.docs.amd.com/en/7.13.0-preview/index.html",
+    "version_list_link": f"https://rocm.docs.amd.com/en/7.13.0-preview/release/versions.html",
+    "nav_secondary_items": {
+        "GitHub": "https://github.com/ROCm/ROCm",
+        "Community": "https://github.com/ROCm/ROCm/discussions",
+        "Blogs": "https://rocm.blogs.amd.com/",
+        "System and Infra Docs": "https://instinct.docs.amd.com/",
+        "Support": "https://github.com/ROCm/ROCm/issues/new/choose",
+    },
     "link_main_doc": False,
 }
 html_title = f"AMD ROCm {ROCM_VERSION}"
